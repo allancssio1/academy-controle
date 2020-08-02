b4de8f990a0662eff8528dd2ce15114a374c8cae
Allan Freitas <allan.cassio1@gmail.com>
2020-08-02 11:12:04 -0300

    terminado modulo 04

--- READMI.md
+++ READMI.md
@@ -2,6 +2,46 @@
 
 Aplicação web para cadastro de instrutores e alunos de academias, com informações básicas para acordos entre eles.
 
-## Tequinologia usada
+## Seguindo as Aulas da Fase 03
 
-Html, CSS e JavaScript para o front-end e nodeJs para o back-end.
+### Módulo 01
+
+1. Estruturando HTML e CSS do projeto;
+2. Colocando imagens e o formulário para apresentação do Instrutor cadastrado;
+3. Separando Rotas;
+4. Conhecendo algumas Middlewares;
+5. Aprofundando no CSS transition.
+
+### Modulo 02
+
+1. Estilizando as dados do instrutor;
+2. Recebendo e trabalhando com dados do Front-end com req.body;
+3. Validando dados no Back-end;
+4. Separando as funções das rotas;
+5. Usando "node fs";
+5. Trabalhando com JSON.
+
+### Módulo 03
+
+1. Estruturando dados do instrutor;
+2. Buscando instrutor por parametros nas rotas;
+3. Estuturando a visualização do instrutor na tela;
+4. Calculando data.
+
+### Módulo 04
+
+1. Utilizando lógica de idade no back-end;
+2. Trazendo pagina com include do Nunjuks;
+3. Fazendo ajuste de datas.
+
+### Módulo 05
+
+...
+
+### Módulo 06
+
+...
+
+### Módulo 07
+
+...
--- instructors.js
+++ instructors.js
@@ -1,5 +1,45 @@
 const fs = require('fs')
 const data = require('./data.json')
+const { age, date } = require('./utils')
+const Intl = require('intl')
+
+// function for show instructors
+exports.show = function (req, res) {
+  const { id } = req.params
+  
+  const foundInstructor = data.instructors.find(function(instructor){
+    return instructor.id == id
+
+  })
+  if(!foundInstructor) return res.send('Instructor not found!')
+
+  const instructor = {
+    ...foundInstructor,
+    age: age (foundInstructor.birth),
+    // gender:"",
+    services: foundInstructor.services.split(","),
+    created_at: Intl.DateTimeFormat('pt-BR').format(foundInstructor.created_at)
+  }
+
+  return res.render('instructors/show', { instructor })
+}
+
+//function for edit instructor
+exports.edit = function (req, res) {
+  const { id } = req.params
+  
+  const foundInstructor = data.instructors.find(function(instructor){
+    return instructor.id == id
+  })
+  if (!foundInstructor) return res.send ('Instructor not found!')
+  
+  const instructor = {
+    ...foundInstructor,
+    birth: date(foundInstructor.birth)
+  }
+
+  return res.render('instructors/edit', { instructor })
+}
 
 //function for create
 exports.post = function (req, res) {
